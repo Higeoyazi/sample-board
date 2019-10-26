fed1e4fb76e51dd365b2d932173324ec97a69707
okamoto ryo <j270549a@gmail.com>
2019-10-26 23:51:42 +0900

    fix_indent

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,26 +3,25 @@
 @section('content')
 
 <div class="card-body">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h5 class="card-title">検索フォーム</h5>
-                <div id="custom-search-input">
-                    <div class="input-group col-md-12">
-                        <form action="{{ route('posts.search') }}" method="get">
-
-                            <input type="text" class="form-control input-lg" placeholder="Buscar" name="search" value="">
-                            <span class="input-group-btn" style="position: relative;top: -36px;right: -179px;">
-                                <button class="btn btn-info" type="submit">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                            </span>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h5 class="card-title">検索フォーム</h5>
+        <div id="custom-search-input">
+          <div class="input-group col-md-12">
+            <form action="{{ route('posts.search') }}" method="get">
+              <input type="text" class="form-control input-lg" placeholder="Buscar" name="search" value="">
+              <span class="input-group-btn" style="position: relative;top: -36px;right: -179px;">
+                <button class="btn btn-info" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+              </span>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 
 <div class="card-header">Board</div>
@@ -46,13 +45,14 @@
           カテゴリー:
           <a href="{{ route('posts.index', ['category_id' => $post->category_id]) }}">
             {{ $post->category->category_name }}
+          </a>
         </h5>
 
         <h5 class="card-title">
-          Tag:
+          タグ:
           @foreach($post->tags as $tag)
             <a href="{{ route('posts.index', ['tag_name' => $tag->tag_name]) }}">
-                #{{ $tag->tag_name }}
+              #{{ $tag->tag_name }}
             </a>
           @endforeach
         </h5>
